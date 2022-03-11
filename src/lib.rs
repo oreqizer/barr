75f4b44960fc9c7f62a93b431a3ff5ba4778da0c
oreqizer <oreqizer@gmail.com>
2022-03-11 20:28:58 +0100

    chore: fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::path::Path;
 use std::fs;
+use std::path::Path;
 
 pub fn barrel_write(filepath: &str, ext: &Option<String>) {
     let path = Path::new(filepath);
@@ -17,18 +17,23 @@ pub fn barrel_write(filepath: &str, ext: &Option<String>) {
     fs::create_dir(&dir).expect("failed to create directory");
     fs::rename(path, dir.join(path.file_name().unwrap())).expect("failed to move file");
 
-    fs::write(dir.join(Path::new("index").with_extension(ext)), content).expect("failed to write file");
+    fs::write(dir.join(Path::new("index").with_extension(ext)), content)
+        .expect("failed to write file");
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::barrel_write;
     use std::fs;
     use std::path::Path;
-    use crate::barrel_write;
 
     fn setup(dir: &str) {
         fs::create_dir_all(dir).unwrap();
-        fs::write(Path::new(dir).join("file.js"), "export default function file() {}\n").unwrap();
+        fs::write(
+            Path::new(dir).join("file.js"),
+            "export default function file() {}\n",
+        )
+        .unwrap();
     }
 
     fn teardown(dir: &str) {
@@ -41,8 +46,14 @@ mod tests {
 
         barrel_write("./tmp/1/file.js", &None);
 
-        assert!(Path::new("./tmp/1/file/file.js").exists(), "expected tmp/1/file/file.js to exist");
-        assert!(Path::new("./tmp/1/file/index.js").exists(), "expected tmp/1/file/index.js to exist");
+        assert!(
+            Path::new("./tmp/1/file/file.js").exists(),
+            "expected tmp/1/file/file.js to exist"
+        );
+        assert!(
+            Path::new("./tmp/1/file/index.js").exists(),
+            "expected tmp/1/file/index.js to exist"
+        );
 
         let index = fs::read_to_string("./tmp/1/file/index.js").unwrap();
 
@@ -57,8 +68,14 @@ mod tests {
 
         barrel_write("./tmp/2/file.js", &Some("ts".to_string()));
 
-        assert!(Path::new("./tmp/2/file/file.js").exists(), "expected tmp/2/file/file.js to exist");
-        assert!(Path::new("./tmp/2/file/index.ts").exists(), "expected tmp/2/file/index.ts to exist");
+        assert!(
+            Path::new("./tmp/2/file/file.js").exists(),
+            "expected tmp/2/file/file.js to exist"
+        );
+        assert!(
+            Path::new("./tmp/2/file/index.ts").exists(),
+            "expected tmp/2/file/index.ts to exist"
+        );
 
         let index = fs::read_to_string("./tmp/2/file/index.ts").unwrap();
 
