8886815e5e4827e0f5eaea63288e7d564c0a9f13
oreqizer <oreqizer@gmail.com>
2022-03-11 15:12:24 +0100

    INIT

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+use std::path::Path;
+use std::fs;
+
+pub fn barrel_write(filepath: &str, ext: &Option<String>) {
+    let path = Path::new(filepath);
+
+    let file = path.file_stem().unwrap().to_str().unwrap();
+    let ext = match ext {
+        Some(e) => e,
+        None => path.extension().unwrap().to_str().unwrap(),
+    };
+
+    let content = format!("export {{ default }} from \"./{}\";\n", file);
+
+    let dir = path.parent().unwrap().join(file);
+
+    fs::create_dir(&dir).expect("failed to create directory");
+    fs::rename(path, dir.join(path.file_name().unwrap())).expect("failed to move file");
+
+    fs::write(dir.join(Path::new("index").with_extension(ext)), content).expect("failed to write file");
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::path::Path;
+    use crate::barrel_write;
+
+    fn setup(dir: &str) {
+        fs::create_dir_all(dir).unwrap();
+        fs::write(Path::new(dir).join("file.js"), "export default function file() {}\n").unwrap();
+    }
+
+    fn teardown(dir: &str) {
+        fs::remove_dir_all(dir).unwrap();
+    }
+
+    #[test]
+    fn write_barrel() {
+        setup("tmp/1");
+
+        barrel_write("./tmp/1/file.js", &None);
+
+        assert!(Path::new("./tmp/1/file/file.js").exists(), "expected tmp/1/file/file.js to exist");
+        assert!(Path::new("./tmp/1/file/index.js").exists(), "expected tmp/1/file/index.js to exist");
+
+        let index = fs::read_to_string("./tmp/1/file/index.js").unwrap();
+
+        assert_eq!(&index, "export { default } from \"./file\";\n");
+
+        teardown("tmp/1");
+    }
+
+    #[test]
+    fn write_barrel_extension() {
+        setup("tmp/2");
+
+        barrel_write("./tmp/2/file.js", &Some("ts".to_string()));
+
+        assert!(Path::new("./tmp/2/file/file.js").exists(), "expected tmp/2/file/file.js to exist");
+        assert!(Path::new("./tmp/2/file/index.ts").exists(), "expected tmp/2/file/index.ts to exist");
+
+        let index = fs::read_to_string("./tmp/2/file/index.ts").unwrap();
+
+        assert_eq!(&index, "export { default } from \"./file\";\n");
+
+        teardown("tmp/2");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+
+use barr::barrel_write;
+
+/// Barrel your JavaScript files! 🛢
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+struct Args {
+    /// The file extension to create
+    #[clap(short, long)]
+    extension: Option<String>,
+
+    /// Prints debug output
+    #[clap(short, long)]
+    debug: bool,
+
+    /// Files to process
+    files: Vec<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for f in args.files {
+        barrel_write(&f, &args.extension);
+    }
+}
